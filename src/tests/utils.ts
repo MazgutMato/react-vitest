5468b62adbb6032234646191d585e7bd1bbd4a11
Mato Mazgut <matej.mazgut@exe.sk>
2025-02-02 16:06:52 +0100

    Auth testing

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,5 +1,6 @@
 import { delay, http, HttpResponse } from "msw";
 import { server } from "./mocks/server";
+import { useAuth0, User } from "@auth0/auth0-react";
 
 export const simulateDelay = (endpoint: string) => {
     server.use(
@@ -16,3 +17,21 @@ export const simulateError = (endpoint: string) => {
     );
 }
 
+type AuthState = {
+    isAuthenticated: boolean;
+    isLoading: boolean;
+    user: User | undefined;
+}
+
+export const mockAuthState = (authState: AuthState) => {
+    vi.mocked(useAuth0).mockReturnValue({
+        ...authState,
+        getAccessTokenSilently: vi.fn().mockResolvedValue('a'),
+        getAccessTokenWithPopup: vi.fn(),
+        getIdTokenClaims: vi.fn(),
+        loginWithRedirect: vi.fn(),
+        loginWithPopup: vi.fn(),
+        logout: vi.fn(),
+        handleRedirectCallback: vi.fn()
+    })
+}
--- vitest-setup.ts
+++ vitest-setup.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom/vitest'
 import ResizeObserver from "resize-observer-polyfill"
 import { afterAll, afterEach, beforeAll, vi } from 'vitest'
 import { server } from './src/tests/mocks/server'
+import { PropsWithChildren, ReactNode } from 'react'
 
 global.ResizeObserver = ResizeObserver
 
@@ -15,6 +16,19 @@ afterEach(() => {
 afterAll(() => {
     server.close()
 })
+
+vi.mock('@auth0/auth0-react', () => {
+    return {
+        useAuth0: vi.fn().mockReturnValue({
+            isAuthenticated: false,
+            isLoading: false,
+            user: undefined
+        }),
+        Auth0Provider: ({ children }: PropsWithChildren) => children,
+        withAuthenticationRequired: (component: ReactNode) => component
+    }
+});
+
 
 window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
